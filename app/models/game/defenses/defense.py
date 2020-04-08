bacb6964b2ee17cc32eeb7b8799f5e7a30d48cd3
Steeve Vandecappelle <me@mizore.fr>
2020-04-08 17:08:29 +0200

    chore(base_objects): Init base object from ideas

---
+++ app/models/game/defenses/defense.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Defense(Enum):
+    FlakCannon = ""
+    MissileBattery = ""
+    LaserArtillery = ""
+    IonArtillery = ""
+    Coilgun = ""
+
---
+++ app/models/game/technologies/technology.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Technology(Enum):
+    Computer = ""
+    Optical = ""
+    Chemistry = ""
+    Alliage = ""
+    Energy = ""
+    Distorsion = ""
+    Atom = ""
